c960b49f8eccd70ec515c95b212b910703cf6847
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-26 17:44:56 -0700

    Add files via upload

--- main.kts
+++ main.kts
@@ -17,5 +17,55 @@ class Candy {
         toblerone = 34
         twix = 24
     }
-
+    fun sourCandy(){
+        nerds = 12
+        trolliCrawlers = 18
+        sourPatch = 19
+        airheads = 11
+        toxicWaste = 11
+        jollyRanchers = 11
+        starburst = 20
+        sweetTarts = 13
+    }
+    fun Gummy(){
+        nerds_gummy_clusters = 22
+        swedishFish = 21
+        trolliCrawlers = 18
+        haribo_gold_bears = 21
+        lifeSavers = 2
+        albanese = 14
+        black_forest_gummybears = 18
+        airheads = 11
+        skittles = 30
+    }
+    fun Caramel(){
+        wertherOriginal = 22
+        kraftAmerica = 26
+        cadburyCaramello = 25
+        dove = 17
+        milkyWay = 14
+        riesen = 15
+        rollo = 4
+        100grand = 22
+        zero = 59
+        ghiradelli = 16
+        payday = 21
+        lindt = 14
+        milkDuds = 20
+    }
+    fun Lollipops(){
+        dumdum = 5
+        tootsiePop = 10
+        ringPop = 11
+        jumbo_push_pop = 10
+        charms_blow_pop = 13
+        zolli_ball_popz = 0
+    }
+    fun hardCandy(){
+        great_value_butterscotch = 10
+        lifeSavers = 2
+        jollyRanchers = 11
+        great_value_strawberry_filled = 10
+        wertherOriginal = 22
+    }
 }
