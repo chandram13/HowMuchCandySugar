d427af4ffcf2e837b11fd27c10d31c410aab9e8d
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-26 17:16:57 -0700

    Add files via upload

---
+++ main.kts
@@ -0,0 +1,21 @@
+
+class Candy {
+    fun majorChocolates(){
+        threeMusketeers = 36
+        almondJoy = 20
+        babyRuth = 28
+        butterfinger = 28
+        hersheyBar = 24
+        cadbury_dairy_milk = 56
+        heath = 24
+        kinder = 54
+        lindor = 43
+        m&ms = 16
+        milkyWay = 14
+        reeses = 17
+        snickers = 28
+        toblerone = 34
+        twix = 24
+    }
+
+}
